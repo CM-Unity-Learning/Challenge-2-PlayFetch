c3f66ac8bf3ed7b18e7711344d6fd762990f2b68
ChulumancoMbete <chulumancombete50@gmail.com>
2024-06-26 10:22:44 +0200

    initial commit
    Unity unit 2 challenge: Play Fetch

---
+++ Assets/Challenge 2/Scripts/DestroyOutOfBoundsX.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestroyOutOfBoundsX : MonoBehaviour
+{
+    //private GameObject dogPrefab;
+    //private GameObject[] ballPrefabs;
+
+    private float leftLimit = 30.0f;
+    private float bottomLimit = 30.0f;
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Destroy dogs if x position less than left limit
+        if (transform.position.x > leftLimit)
+        {
+            Destroy(gameObject);
+        } 
+        // Destroy balls if y position is less than bottomLimit
+        else if (transform.position.x < -bottomLimit)
+        {
+            Destroy(gameObject);
+        }
+
+    }
+}
